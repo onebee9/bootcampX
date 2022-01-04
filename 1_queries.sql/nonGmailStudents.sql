18f113ea10423e1dcfcbeb225aec889fb62d019d
ibifuro odu <oduibifuro@gmail.com>
2022-01-04 17:38:49 -0500

    new queries for assignments

--- 1_queries.sql/nonGmailStudents.sql
+++ 1_queries.sql/nonGmailStudents.sql
@@ -2,4 +2,3 @@ SELECT name, email, id, and cohort_id
 FROM students
 WHERE email NOT LIKE '%gmail.com'
 AND phone IS NULL;
-
--- 2_queries_joins/cohortAssignmentDuration.sql
+++ 2_queries_joins/cohortAssignmentDuration.sql
@@ -1,3 +1,4 @@
 SELECT SUM(A.duration) as total_duration
 FROM assignment_submissions A
 INNER JOIN students B ON A.student_id = B.id;
+WHERE cohorts.name = 'FEB12';
