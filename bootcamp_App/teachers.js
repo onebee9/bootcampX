6f876f7ad63a4588073699c2c4a73048aacd6105
ibifuro odu <oduibifuro@gmail.com>
2022-02-12 15:47:17 -0500

    updated to use query parameters

--- bootcamp_App/teachers.js
+++ bootcamp_App/teachers.js
@@ -6,14 +6,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
-ORDER BY teacher;`).then(res => {
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`
+
+const cohortName = process.argv[2];
+const values = [cohortName];
+
+pool.query(queryString,values).then(res => {
   console.log(res);
 })
 .catch(err => console.error('query error', err.stack));
